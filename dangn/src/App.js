d9a81be1916c188f0981444f4455ce3d4e848d52
yhsul <yhsul@nshc.net>
2020-08-26 09:07:33 +0900

    리액트 라우터 안되서 코드 통째로 올림

---
+++ dangn/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./main";
+
+export default function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
+  );
+}
