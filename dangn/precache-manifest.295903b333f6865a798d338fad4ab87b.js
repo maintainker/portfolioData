74ccbcb323953086ffd0b5eb7ec8f03836022672
yhsul <yhsul@nshc.net>
2020-08-27 10:06:45 +0900

    해쉬 라우터 시도 1

---
+++ dangn/precache-manifest.295903b333f6865a798d338fad4ab87b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebbbc8561887bea3cb9ae5926762148",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f95509d0b432d46d35d7",
+    "url": "./static/css/2.ab84ab19.chunk.css"
+  },
+  {
+    "revision": "f95509d0b432d46d35d7",
+    "url": "./static/js/2.828bb2a5.chunk.js"
+  },
+  {
+    "revision": "823099be0383bbe77bdfe48639fe2996",
+    "url": "./static/js/2.828bb2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4377820a27fb671caa9",
+    "url": "./static/js/main.a6395096.chunk.js"
+  },
+  {
+    "revision": "d3878d01065e028a338a",
+    "url": "./static/js/runtime-main.9ddeaba2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
--- dangn/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Routes from "./Routes";
-import "./index.css";
-
-render(<Routes />, document.getElementById("root"));
